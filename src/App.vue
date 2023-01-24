15629e6218dd8fcce60bb0bdc5d6b836478b11c0
Alynne Fereira <alynneferreiras@gmail.com>
2023-01-24 12:33:51 -0300

    put the response in div

--- src/App.vue
+++ src/App.vue
@@ -9,48 +9,53 @@ import HelloWorld from './components/HelloWorld.vue'
     <br />
 
     <input v-model="name" placeholder="search here" />
-    
     <div>Selected: {{ selected }}</div>
-
     <select v-model="selected">
       <option disabled value="">Please select one</option>
-      <option>People</option>
-      <option>Films</option>
-      <option>Planets</option>
-      <option>Starships</option>
-      <option>All</option>
+      <option>people</option>
+      <option>films</option>
+      <option>planets</option>
+      <option>starships</option>
+      <option>all</option>
     </select>
 
-    <button @click="search">Search</button>
-
+    <button @click="getItemsAndRecommedatios()">Search</button>
+    
+    <!-- respose of get -->
+    <div v-for="item in items_rec.item"> <!--:key="p.name">-->
+        <h2>{{ item.name }}</h2>
+    </div>
 
   </WelcomeItem>
 </template>
 
 <script>
-
 export default {
   data() {
     return {
-      selected: 'All',
+      selected: 'all',
       options: [
-        { text: 'People', value: 'People' },
-        { text: 'Films', value: 'Films' },
-        { text: 'Planets', value: 'Planets' },
-        { text: 'Starships', value: 'Starships' },
-        { text: 'All', value: 'All' }
+        { text: 'people', value: 'people' },
+        { text: 'films', value: 'films' },
+        { text: 'planets', value: 'planets' },
+        { text: 'starships', value: 'starships' },
+        { text: 'all', value: 'all' }
       ],
-      name: ''
+      name: '',
+      items_rec: []
     }
   },
   methods: {
-    search(event) {
-      
-      console.log(`Hello ${this.name}, ${this.selected}!`);
-      fetch("http://127.0.0.1:5000/people/luke")
+  
+    getItemsAndRecommedatios() {
+      // TODO: remove url hard coded -->
+      fetch(`http://127.0.0.1:5000/${this.selected}/${this.name}`)
         .then(response => response.json())
-        .then(data => console.log(data));
+        .then(data => this.items_rec = data)
     }
+  },
+  click() {
+    this.getItemsAndRecommedatios()
   }
 }
 
